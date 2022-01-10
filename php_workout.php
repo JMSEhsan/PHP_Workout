a021a81b0e3b0488bbb6adc4f76c6f1df8e6cae8
Ehsan <JMSEhsan@gmail.com>
2022-01-09 23:29:45 -0700

    commit

--- php_workout.php
+++ php_workout.php
@@ -17,6 +17,14 @@ echo "I love $txt!";
 $x = 5 /* + 15 */ + 5 * 6;
 echo $x;
 ?>
+                     <!-- php math -->
+<?php
+echo(pi()); // returns 3.1415926535898
+?>
 
+<?php
+echo(min(0, 400, 30, 200, -30, -60));  // returns -60
+echo(max(0, 400, 30, 200, -30, -60));  // returns 400
+?>
 </body>
 </html>
