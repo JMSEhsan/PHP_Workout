57d3f44ee1ed363a1460d33fccbc48d3e160f9dd
Ehsan <JMSEhsan@gmail.com>
2022-01-14 23:47:22 -0700

    commit, add more practice

--- php_workout.php
+++ php_workout.php
@@ -27,5 +27,14 @@ echo(min(0, 400, 30, 200, -30, -60). "\n"); // returns -60
 echo(max(0, 400, 30, 200, -30, -60). "\n");  // returns 400
 ?>
 
+         <!-- Chieck if the type of a variable is integer -->
+<?php
+$x = 3478;
+var_dump(is_int($x));
+
+$x = 34.09;
+var_dump(is_int($x));   // var_dump() function in PHP returns the data type and value
+?>
+
 </body>
 </html>
